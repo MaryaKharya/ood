2e84f47524ec60a742b1f14200d2534ddae4480f
MaryaKharya <korcemkinamaria76@gmail.com>
2021-09-19 14:02:48 +0300

    add dancing

--- SimUDuck/SimUDuck/SimUDuck.cpp
+++ SimUDuck/SimUDuck/SimUDuck.cpp
@@ -1,20 +1,227 @@
-﻿// SimUDuck.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <cassert>
 #include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+struct IFlyBehavior
+{
+	virtual ~IFlyBehavior() {};
+	virtual void Fly() = 0;
+};
+class FlyWithWings : public IFlyBehavior
+{
+public:
+	void Fly() override
+	{
+		cout << "I'm flying with wings!!" << endl;
+	}
+};
+
+class FlyNoWay : public IFlyBehavior
+{
+public:
+	void Fly() override {}
+};
+
+struct IQuackBehavior
+{
+	virtual ~IQuackBehavior() {};
+	virtual void Quack() = 0;
+};
+
+class QuackBehavior : public IQuackBehavior
+{
+public:
+	void Quack() override
+	{
+		cout << "Quack Quack!!!" << endl;
+	}
+};
+class SqueakBehavior : public IQuackBehavior
+{
+public:
+	void Quack() override
+	{
+		cout << "Squeek!!!" << endl;
+	}
+};
+
+class MuteQuackBehavior : public IQuackBehavior
+{
+public:
+	void Quack() override {}
+};
+
+class IDanceBehavior
+{
+public:
+	virtual ~IDanceBehavior() {}
+	virtual void Dance() = 0;
+};
+
+class DanceMinuet : public IDanceBehavior
+{
+public:
+	void Dance() override
+	{
+		std::cout << "I'm dancing minuet!\n";
+	}
+};
+
+class DanceWaltz : public IDanceBehavior
+{
+public:
+	void Dance() override
+	{
+		std::cout << "I'm dancing waltz!\n";
+	}
+};
+
+class DanceNoWay : public IDanceBehavior
+{
+public:
+	void Dance() override {}
+};
+
+class Duck
+{
+public:
+	Duck(unique_ptr<IFlyBehavior>&& flyBehavior,
+		unique_ptr<IQuackBehavior>&& quackBehavior,
+		unique_ptr<IDanceBehavior>&& danceBehavior)
+		: m_quackBehavior(move(quackBehavior))
+		, m_danceBehavior(move(danceBehavior))
+	{
+		assert(m_quackBehavior);
+		assert(m_danceBehavior);
+		SetFlyBehavior(move(flyBehavior));
+	}
+	void Quack() const
+	{
+		m_quackBehavior->Quack();
+	}
+	void Swim()
+	{
+		cout << "I'm swimming" << endl;
+	}
+	void Fly()
+	{
+		m_flyBehavior->Fly();
+	}
+	void Dance() const
+	{
+		m_danceBehavior->Dance();
+	}
+	void SetFlyBehavior(unique_ptr<IFlyBehavior>&& flyBehavior)
+	{
+		assert(flyBehavior);
+		m_flyBehavior = move(flyBehavior);
+	}
+	virtual void Display() const = 0;
+	virtual ~Duck() = default;
+
+private:
+	unique_ptr<IFlyBehavior> m_flyBehavior;
+	unique_ptr<IQuackBehavior> m_quackBehavior;
+	unique_ptr<IDanceBehavior> m_danceBehavior;
+};
+
+class MallardDuck : public Duck
+{
+public:
+	MallardDuck()
+		: Duck(make_unique<FlyWithWings>(), make_unique<QuackBehavior>(), make_unique<DanceMinuet>())
+	{
+	}
+
+	void Display() const override
+	{
+		cout << "I'm mallard duck" << endl;
+	}
+};
+
+class RedheadDuck : public Duck
+{
+public:
+	RedheadDuck()
+		: Duck(make_unique<FlyWithWings>(), make_unique<QuackBehavior>(), make_unique<DanceWaltz>())
+	{
+	}
+	void Display() const override
+	{
+		cout << "I'm redhead duck" << endl;
+	}
+};
+class DecoyDuck : public Duck
+{
+public:
+	DecoyDuck()
+		: Duck(make_unique<FlyNoWay>(), make_unique<MuteQuackBehavior>(), make_unique<DanceNoWay>())
+	{
+	}
+	void Display() const override
+	{
+		cout << "I'm decoy duck" << endl;
+	}
+};
+class RubberDuck : public Duck
+{
+public:
+	RubberDuck()
+		: Duck(make_unique<FlyNoWay>(), make_unique<SqueakBehavior>(), make_unique<DanceNoWay>())
+	{
+	}
+	void Display() const override
+	{
+		cout << "I'm rubber duck" << endl;
+	}
+};
+
+class ModelDuck : public Duck
+{
+public:
+	ModelDuck()
+		: Duck(make_unique<FlyNoWay>(), make_unique<QuackBehavior>(), make_unique<DanceNoWay>())
+	{
+	}
+	void Display() const override
+	{
+		cout << "I'm model duck" << endl;
+	}
+};
+
+void DrawDuck(Duck const& duck)
+{
+	duck.Display();
+}
+
+void PlayWithDuck(Duck& duck)
+{
+	DrawDuck(duck);
+	duck.Quack();
+	duck.Fly();
+	duck.Dance();
+	cout << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	MallardDuck mallardDuck;
+	PlayWithDuck(mallardDuck);
+
+	RedheadDuck redheadDuck;
+	PlayWithDuck(redheadDuck);
+
+	RubberDuck rubberDuck;
+	PlayWithDuck(rubberDuck);
+
+	DecoyDuck decoyDuck;
+	PlayWithDuck(decoyDuck);
+
+	ModelDuck modelDuck;
+	PlayWithDuck(modelDuck);
+	modelDuck.SetFlyBehavior(make_unique<FlyWithWings>());
+	PlayWithDuck(modelDuck);
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
